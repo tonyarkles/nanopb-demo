68f2bf2dba670eb0b7c5b78763addef045983cba
Tony Arkles <tony@persea.ca>
2023-10-09 09:50:55 -0600

    working

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <pb_encode.h>
+#include "src/demo.pb.h"
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main() {
+  uint8_t buffer[Hello_size];
+  
   while(1) {
-    k_msleep(100);
+    Hello hello_msg = Hello_init_zero;
+    hello_msg.time = k_uptime_get();
+    hello_msg.val = 42;
+
+    pb_ostream_t stream = pb_ostream_from_buffer(buffer, Hello_size);
+    bool status = pb_encode(&stream, Hello_fields, &hello_msg);
+    size_t len = stream.bytes_written;
+
+    LOG_INF("wrote %d bytes to output buffer, status %d", len, status);
+    k_msleep(1000);
   }
 }
